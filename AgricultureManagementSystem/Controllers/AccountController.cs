8d88af487010480dc2001897fb61508b9539b0f0
Samuel <koaladaydreamer@gmail.com>
2021-10-15 16:12:00 +0800

    新增用戶編輯、刪除機制

--- AgricultureManagementSystem/Controllers/AccountController.cs
+++ AgricultureManagementSystem/Controllers/AccountController.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.AspNet.Identity.Owin;
+﻿using AgricultureManagementSystem.Models;
+using AgricultureManagementSystem.ViewModels;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -32,10 +36,82 @@ namespace AgricultureManagementSystem.Controllers
 
         // GET: Edit
         [HttpGet]
-        public ActionResult Edit()
+        public ActionResult Edit(string id)
         {
-            return View();
+            if (string.IsNullOrEmpty(id) || string.IsNullOrWhiteSpace(id))
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+
+            User _user = UserManager.FindById(id);
+            if (_user == null)
+            {
+                return HttpNotFound();
+            }
+
+            var editUserViewModel = new EditUserViewModel()
+            {
+                Account = _user.Account,
+                Email = _user.Email,
+                Region = _user.Region
+            };
+            return View(editUserViewModel);
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(string id, EditUserViewModel editUserViewModel)
+        {
+            if (ModelState.IsValid && !string.IsNullOrEmpty(id))
+            {
+                User _user = UserManager.FindById(id);
+                _user.Account = editUserViewModel.Account;
+                _user.Email = editUserViewModel.Email;
+                _user.Region = editUserViewModel.Region;
+
+                var result = await UserManager.UpdateAsync(_user);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Account");                    
+                }
+                AddErrors(result);
+            }
+            return View(editUserViewModel);
+        }
+
+
+
+
+        [HttpGet]
+        public async Task<ActionResult> Delete(string id)
+        {
+            if (string.IsNullOrEmpty(id) || string.IsNullOrWhiteSpace(id))
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+
+            User _user = UserManager.FindById(id);
+            if (_user == null)
+            {
+                return HttpNotFound();
+            }
+
+            var result = await UserManager.DeleteAsync(_user);
+            if (!result.Succeeded)
+            {
+                AddErrors(result);
+            }
+            return RedirectToAction("Index", "Account");
+        }
+
+        private void AddErrors(IdentityResult identityResult)
+        {
+            foreach (var error in identityResult.Errors)
+            {
+                ModelState.AddModelError("", error);
+            }
+        }
+
 
     }
 }
---
+++ AgricultureManagementSystem/ViewModels/EditUserViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AgricultureManagementSystem.ViewModels
+{
+    public class EditUserViewModel
+    {
+        [Required]
+        [Display(Name = "帳號")]
+        public string Account { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "信箱")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "外場")]
+        public string Region { get; set; }
+
+    }
+}
